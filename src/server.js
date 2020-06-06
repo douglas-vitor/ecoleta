e783f0239cc2af62c19f5f6f79e963cb30fef333
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-06-06 08:55:20 -0300

    Next_Level_Week 05/06 - FINAL

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const server = express()
 
+// Resgatando BD
+const db = require("./database/db")
+
 // Configurando pasta publica
 server.use(express.static("public"))
+
+// Habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({extended: true}))
 
 // Utilizando template engine
 const nunJucks = require("nunjucks")
@@ -19,12 +25,67 @@ server.get("/", (req, res) => {
 
 // Create-point
 server.get("/create-point", (req, res) => {
+    //console.log(req.query)
     return res.render("create-point.html")
 } )
 
+server.post("/savepoint", (req, res) => {
+    // Inserir dados no BD
+    const query = `
+INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+) VALUES (?,?,?,?,?,?,?);
+`
+
+const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+]
+
+function afterInsertData(err) {
+    if(err) {
+        console.log(err)
+        return res.send("Erro no cadastro.")
+    }
+    console.log("Cadastrado com sucesso.")
+    console.log(this)
+    return res.render("create-point.html", {saved: true})
+
+}
+    db.run(query, values, afterInsertData)    
+
+})
+
 // Search-results
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+    if(search == "") {
+        // Pesquisa vazia
+        return res.render("search-results.html", { total: 0})
+    }
+
+    // Pegar dados do BD
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+        const total = rows.length
+        // Mostra a pagina html com os dados do BD
+        return res.render("search-results.html", { places : rows, total: total})
+    })
+
 } )
 
 // Ligar servidor
